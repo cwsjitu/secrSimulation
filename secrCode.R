4c910d692d7638c80a52fcc1dc1d0df9fea2ea7e
cwsjitu <jitu.cws@gmail.com>
2017-05-04 11:15:34 +0530

    Update secrCode.R

--- secrCode.R
+++ secrCode.R
@@ -1,3 +1,6 @@
+##set your working directory with 
+# setwd()
+
 library(secr)
 library(raster)
 library(rgdal)
@@ -6,6 +9,13 @@ library(maptools)
 library(rgeos)
 library(dplyr)
 
-# coordinate system
-latlong = "+init=epsg:4326" # LatLon Projection
-ingrid = "+init=epsg:32643" # UTM Projection
+## coordinate system
+latlong = "+init=epsg:4326" ## LatLon Projection
+ingrid = "+init=epsg:32643" ## UTM Projection
+
+## assumptions to generate capture history
+ N <-          ## Number of individuals/activity centers      
+ traploc <-    ## Number of trap locations
+ g0 <-         ## value between 0.1 and 0.3
+ sigma <-      ## depends on home range of species
+ occasions <- 10  ## 10 sampling occasions  
